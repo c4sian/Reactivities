895574b8f627539e5a894e96fd8f52fea589764c
c4sian <casian.stirbe@yahoo.com>
2025-11-09 17:15:03 +0200

    End of section 15

--- client/src/features/activities/details/ActivityDetailsInfo.tsx
+++ client/src/features/activities/details/ActivityDetailsInfo.tsx
@@ -1,7 +1,6 @@
 import { CalendarToday, Info, Place } from "@mui/icons-material";
 import { Box, Button, Divider, Grid, Paper, Typography } from "@mui/material";
 import { formatDate } from "../../../lib/util/util";
-import type { Activity } from "../../../lib/types";
 import MapComponent from "../../../app/shared/components/MapComponent";
 import { useState } from "react";
 
@@ -42,7 +41,7 @@ export default function ActivityDetailsInfo({ activity }: Props) {
                     <Typography>
                         {activity.venue}, {activity.city}
                     </Typography>
-                    <Button onClick={() => setMapOpen(!mapOpen)}>
+                    <Button onClick={() => setMapOpen(!mapOpen)} sx={{ whiteSpace: 'nowrap', mx: 2 }}>
                         {mapOpen ? 'Hide map' : 'Show map'}
                     </Button>
                 </Grid>
